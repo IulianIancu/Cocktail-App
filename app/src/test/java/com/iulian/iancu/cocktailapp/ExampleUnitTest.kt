41c6cfaab3a708810a83c763e2e05b65fa0710ee
Iulian Iancu <Iulianiancu93@gmail.com>
2022-11-10 13:04:28 +0000

    Add Unit tests

--- app/src/test/java/com/iulian/iancu/cocktailapp/ExampleUnitTest.kt
+++ app/src/test/java/com/iulian/iancu/cocktailapp/ExampleUnitTest.kt
@@ -1,8 +1,29 @@
+@file:OptIn(DelicateCoroutinesApi::class, ExperimentalCoroutinesApi::class)
+
 package com.iulian.iancu.cocktailapp
 
+import com.iulian.iancu.cocktailapp.ui.main.MainViewModel
+import com.iulian.iancu.data.Drink
+import com.iulian.iancu.data.Drinks
+import com.iulian.iancu.data.DrinksRepositoryImpl
+import com.iulian.iancu.data.DrinksService
+import com.iulian.iancu.domain.GetDrinksUseCase
+import com.iulian.iancu.entity.Cocktail
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.Assert
+import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
-
-import org.junit.Assert.*
+import retrofit2.Response
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +31,46 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+    private lateinit var viewModel: MainViewModel
+    private lateinit var getDrinksUseCase: GetDrinksUseCase
+    private lateinit var drinksRepository: DrinksRepositoryImpl
+
+    @MockK
+    lateinit var drinksService: DrinksService
+
+    private val mainThreadSurrogate = newSingleThreadContext("UI thread")
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this, relaxUnitFun = true) // turn relaxUnitFun on for all mocks
+        Dispatchers.setMain(mainThreadSurrogate)
+        drinksRepository = DrinksRepositoryImpl(drinksService)
+        getDrinksUseCase = GetDrinksUseCase(drinksRepository)
+        viewModel = MainViewModel(getDrinksUseCase)
+    }
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun givenSearchIsBlankAndCallSucceeds_usecaseGivesCorrectList() = runTest {
+        val testDrink = Drink(
+            idDrink = "1",
+            strDrink = "1",
+            strDrinkThumb = "1",
+            null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,
+        )
+
+        val testResult = Cocktail("1","1","1","", emptyList())
+        coEvery { drinksService.getDrinks("") } returns Response.success(
+            Drinks(
+                listOf(
+                    testDrink
+                )
+            )
+        )
+
+        val result = getDrinksUseCase("")
+        coVerify { drinksRepository.getDrinks("") }
+        coVerify { drinksService.getDrinks("") }
+
+        Assert.assertEquals(testResult,result)
     }
 }
